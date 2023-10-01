12b74eb38cfedf60e4722ecb4c4f35a935138b6f
Felirian <hhghv49@gmail.com>
2023-10-01 15:49:22 +0300

    add: fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 import {ApolloProvider} from "@apollo/client";
 import { client } from "@/services/gqlService";
+import '../styles/style.scss'
 
 const _App = ({ Component, pageProps }) => {
   return (
