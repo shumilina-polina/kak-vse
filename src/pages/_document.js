23bb70dc5eda2c937918252e2349ecf439ca9e63
Felirian <hhghv49@gmail.com>
2023-10-08 15:14:03 +0300

    add: favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,40 +16,18 @@ class MyDocument extends Document {
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
             rel="stylesheet"
           />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="assets/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="assets/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="assets/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="assets/favicon/site.webmanifest"/>
-          <link
-            rel="mask-icon"
-            href="assets/favicon/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
+
           <meta name="msapplication-TileColor" content="#2b5797"/>
           <meta name="theme-color" content="#000000"/>
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
           />
-          <link rel="apple-touch-icon" sizes="72x72" href="assets/favicon/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png"/>
-          <link rel="manifest" href="assets/favicon/site.webmanifest"/>
-          <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+          <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon/site.webmanifest"/>
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
           <meta name="msapplication-TileColor" content="#da532c"/>
           <meta name="theme-color" content="#ffffff"/>
 
