6d948ebb340d8fb088c224dd2ba44ba66c5bba30
Felirian <hhghv49@gmail.com>
2023-10-11 13:44:43 +0300

    fix: x-ross rendering

--- src/components/CategoryPage/CategoryPage.js
+++ src/components/CategoryPage/CategoryPage.js
@@ -4,29 +4,28 @@ import CardArticle from "@/components/cardArticle/CardArticle";
 import CardVideo from "@/components/cardVideo/CardVideo";
 import {useMediaQuery} from "@mui/material";
 import {apiUrl} from "@/shared/constants/config";
+
 const CategoryPage = ({Category, VIDEO_DATA, ARTICLE_DATA, loaded}) => {
   const isMobile = useMediaQuery(`(max-width: 480px)`);
-  // useEffect(() => {
-  //   let a = VIDEO_DATA
-  //   let b = ARTICLE_DATA
-  //   let s = Math.max(a.length, b.length)*2,
-  //     d = [a,b],
-  //     r = [],
-  //     v, i=-1;
-  //
-  //
-  //   while(s-i++){
-  //     v =  d[i%2][(i-i%2)/2];        // или d[i%2][~~(i/2)]
-  //     if(v !== undefined) r.push(v);
-  //   }
-  //   //console.log(r)
-  // }, []);
 
-  return (
-    <div className={s.category}>
-      <div className={s.category_articles}>
-        {ARTICLE_DATA?.map(
-            (article, index)=>(
+  function arrayConcat(a, b) {
+    let result = [];
+    let len = a?.length >= b?.length ? a?.length : b?.length;
+    for (let i = 0; i < len; i++) {
+      if (a[i])
+        result.push(a[i]);
+      if (b[i])
+        result.push(b[i]);
+    }
+    return result;
+  }
+
+  if (!isMobile) {
+    return ( //-------------------------------------------------desktop
+      <div className={s.category}>
+        <div className={s.category_articles}>
+          {ARTICLE_DATA?.map(
+            (article, index) => (
               <CardArticle
                 Category={Category}
                 author={article?.attributes.author_name}
@@ -37,23 +36,59 @@ const CategoryPage = ({Category, VIDEO_DATA, ARTICLE_DATA, loaded}) => {
                 loaded={loaded}
                 key={`article${index}`}
               />
-        ))}
-      </div>
+            ))}
+        </div>
 
-      <div className={s.category_videos}>
-        {VIDEO_DATA?.map((path, i) => (
-          <CardVideo
-            Category={Category}
-            tags={path.attributes.tags}
-            title={path.attributes.title}
-            url={path.attributes.urlEmbed}
-            loaded={loaded}
-            key={i}
-          />
-        ))}
+        <div className={s.category_videos}>
+          {VIDEO_DATA?.map((path, i) => (
+            <CardVideo
+              Category={Category}
+              tags={path.attributes.tags}
+              title={path.attributes.title}
+              url={path.attributes.urlEmbed}
+              loaded={loaded}
+              key={i}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return ( //-------------------------------------------------mobile
+      <div className={s.category}>
+        {arrayConcat(VIDEO_DATA, ARTICLE_DATA).map((elem, index) => {
+          console.log(elem.__typename);
+          if (elem.__typename === 'VideoEntity') {
+            return (
+              <CardVideo
+                Category={Category}
+                tags={elem.attributes.tags}
+                title={elem.attributes.title}
+                url={elem.attributes.urlEmbed}
+                loaded={loaded}
+                key={index}
+              />
+            );
+          } else {
+            return (
+              <CardArticle
+                Category={Category}
+                author={elem.attributes.author_name}
+                image={apiUrl + elem.attributes.author_photo.data.attributes.url}
+                title={elem.attributes.title}
+                previewText={elem.attributes.content}
+                url={elem.attributes.slug}
+                loaded={loaded}
+                key={index}
+              />
+            );
+          }
+
+        })}
+      </div>
+    );
+  }
+
 };
 
 export default CategoryPage;
