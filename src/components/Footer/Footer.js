094db1f6f503beb8a9f7ceee220e23c940a890f6
Polina Shumilina <shumilina246@gmail.com>
2023-10-31 16:22:09 +0300

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,48 @@
-import React, {useContext, useEffect} from 'react';
-import s from './footer.module.scss'
+import React, { useContext, useEffect } from "react";
+import s from "./footer.module.scss";
 import SvgSelector from "@/components/SvgSelector";
-import {colorContext, sizeContext} from "@/components/Context";
+import { colorContext, sizeContext } from "@/components/Context";
+import classNames from "classnames";
+import { useMediaQuery } from "@mui/material";
 const Footer = () => {
-  const nowDate = new Date()
-  const [colorVersion, setColorVersion] = useContext(colorContext)
-  const [sizeVersion, setSizeVersion] = useContext(sizeContext)
+  const nowDate = new Date();
+  const [colorVersion, setColorVersion] = useContext(colorContext);
+  const [sizeVersion, setSizeVersion] = useContext(sizeContext);
 
-  const scrollToTop = () => (window.scrollTo(0, 0))
+  const scrollToTop = () => window.scrollTo(0, 0);
 
+  const isMobile = useMediaQuery(`(max-width: 480px)`);
 
   return (
     <div className={s.wrapper}>
       <footer>
-        <button onClick={()=>(scrollToTop())}>
-          <SvgSelector svg={'footer-logo'} />
-        </button>
+        <div className={s.logo_wr}>
+          <button onClick={() => scrollToTop()}>
+            <SvgSelector svg={"footer-logo"} />
+          </button>
+          {isMobile || (
+            <p className={`${sizeVersion}_label`}>©{nowDate.getFullYear()}</p>
+          )}
+        </div>
 
         <div className={s.social}>
-          <a href={'https://vk.com/kakvseinfo'} target={'_blank'}>
-            <SvgSelector svg={'footer-vk'}/>
+          <a href={"https://vk.com/kakvseinfo"} target={"_blank"}>
+            <SvgSelector svg={"footer-vk"} />
           </a>
-          <a href={'https://dzen.ru/kakvse'} target={'_blank'}>
-            <SvgSelector svg={'footer-dzen'}/>
+          <a href={"https://dzen.ru/kakvse"} target={"_blank"}>
+            <SvgSelector svg={"footer-dzen"} />
           </a>
-          <a href={'https://rutube.ru/channel/31625794/'} target={'_blank'}>
-            <SvgSelector svg={'footer-rutube'}/>
+          <a href={"https://rutube.ru/channel/31625794/"} target={"_blank"}>
+            <SvgSelector svg={"footer-rutube"} />
           </a>
         </div>
-        <p className={`${sizeVersion}_label`}>©{nowDate.getFullYear()}</p>
+        {isMobile && (
+          <p className={`${sizeVersion}_label`}>©{nowDate.getFullYear()}</p>
+        )}
+        <div className={classNames(`${sizeVersion}_label`, s.podderzhka)}>
+          <span>При поддержке</span>
+          <SvgSelector svg={"footer-logo-iri"} />
+        </div>
       </footer>
     </div>
   );
