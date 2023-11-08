86ba343e1f7df1963d6af20a282162496c5177bd
Polina Shumilina <shumilina246@gmail.com>
2023-11-08 19:55:43 +0300

    fix limit

--- src/services/gqlService.js
+++ src/services/gqlService.js
@@ -8,287 +8,281 @@ export const client = new ApolloClient({
 });
 
 export const GET_CATEGORY = gql`
-query ($category: String) {  
-  categoryVideos: videos (  
-    sort: "createdAt:desc"   
-    filters: {category: {eq: $category}}
-   ) { 
-    data {
-      attributes {
-        title
-        category 
-        urlEmbed
-        tags
-        urlShare        
-      }
-    }
-  }
-  categoryArticles: articles (    
-    sort: "createdAt:desc" 
-    filters: {category: {eq: $category}}
-   ) { 
-    data {      
-      attributes {
-        slug
-        title        
-        category
-        content
-        image {
-          data {
-            attributes {
-              url
-              blurhash
-            }
-          }
-        }
-        author_name
-        author_photo {
-          data {
-            attributes {
-              url
-              hash
-            }
-          }
+  query ($category: String) {
+    categoryVideos: videos(
+      pagination: { limit: 999 }
+      sort: "createdAt:desc"
+      filters: { category: { eq: $category } }
+    ) {
+      data {
+        attributes {
+          title
+          category
+          urlEmbed
+          tags
+          urlShare
         }
       }
     }
-  }
-}
-`;
-
-export const GET_ARTICLE = gql `
-query ($slug: String) {  
-  articleID: articles (      
-    filters: {slug: {eq: $slug}}
-  ) {
-    data {
-      id
-      attributes {
-        image {
-          data {
-            attributes {
-              url
-            }
-          }
-        }
-        author_name
-        author_photo {
-          data {
-            attributes {
-              url
-            }
-          }
-        }
-        title
-        content 
-        slug
-        category
-        label_content {
+    categoryArticles: articles(
+      pagination: { limit: 999 }
+      sort: "createdAt:desc"
+      filters: { category: { eq: $category } }
+    ) {
+      data {
+        attributes {
+          slug
+          title
+          category
           content
-          link_vk
-          phone
-          link_multicentr
-        }
-      }
-    }
-  }
-}
-`;
-
-export const GET_ANSWER = gql `
-query ($slug: String) {  
-  answerID: faqs (
-    filters: {slug: {eq: $slug}}
-  ) {
-    data {
-      attributes {
-        slug
-        title
-        content
-      }
-    }
-  }
-}
-`;
-
-export const GET_FAQS = gql `
-query {  
-  faqCategories {
-    data {
-      attributes {
-        title
-        faqs (sort: "createdAt:desc") {
-          data {
-            attributes {
-              title 
-              slug             
+          image {
+            data {
+              attributes {
+                url
+                blurhash
+              }
+            }
+          }
+          author_name
+          author_photo {
+            data {
+              attributes {
+                url
+                hash
+              }
             }
           }
         }
       }
     }
-  }  
-}
+  }
+`;
+
+export const GET_ARTICLE = gql`
+  query ($slug: String) {
+    articleID: articles(filters: { slug: { eq: $slug } }) {
+      data {
+        id
+        attributes {
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+          author_name
+          author_photo {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+          title
+          content
+          slug
+          category
+          label_content {
+            content
+            link_vk
+            phone
+            link_multicentr
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ANSWER = gql`
+  query ($slug: String) {
+    answerID: faqs(filters: { slug: { eq: $slug } }) {
+      data {
+        attributes {
+          slug
+          title
+          content
+        }
+      }
+    }
+  }
+`;
+
+export const GET_FAQS = gql`
+  query {
+    faqCategories {
+      data {
+        attributes {
+          title
+          faqs(sort: "createdAt:desc", pagination: { limit: 199 }) {
+            data {
+              attributes {
+                title
+                slug
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 `;
 
 export const GET_DATA_INDEX = gql`
-query {  
-  advicesVideos: videos (
-    sort: "createdAt:desc"
-    pagination: {limit: 2}   
-    filters: {category: {eq: "advices"}}
-  ) { 
-    data {
-      attributes {
-        title
-        category 
-        urlEmbed
-        tags
-        urlShare
-        onMainPage 
-      }
-    }
-  }
-  advicesArticles: articles ( 
-    sort: "createdAt:desc"
-    pagination: {limit: 3}   
-    filters: {category: {eq: "advices"}}
-  ) { 
-    data {
-      id
-      attributes {
-        slug
-        title        
-        category
-        image {
-          data {
-            attributes {
-              url
-              blurhash
-            }
-          }
-        }
-        author_name
-        author_photo {
-          data {
-            attributes {
-              url
-              blurhash
-            }
-          }
+  query {
+    advicesVideos: videos(
+      sort: "createdAt:desc"
+      pagination: { limit: 2 }
+      filters: { category: { eq: "advices" } }
+    ) {
+      data {
+        attributes {
+          title
+          category
+          urlEmbed
+          tags
+          urlShare
+          onMainPage
         }
       }
     }
-  }
-  opportunitiesVideos: videos ( 
-    sort: "createdAt:desc"
-    pagination: {limit: 2}   
-    filters: {category: {eq: "opportunities"}}
-  ) { 
-    data {
-      attributes {
-        title
-        category 
-        urlEmbed
-        tags
-        urlShare
-        onMainPage 
-      }
-    }
-  }
-  opportunitiesArticles: articles ( 
-    sort: "createdAt:desc"
-    pagination: {limit: 3}   
-    filters: {category: {eq: "opportunities"}}
-  ) { 
-    data {
-      id
-      attributes {
-        slug
-        title        
-        category
-        image {
-          data {
-            attributes {
-              url
-              blurhash
+    advicesArticles: articles(
+      sort: "createdAt:desc"
+      pagination: { limit: 3 }
+      filters: { category: { eq: "advices" } }
+    ) {
+      data {
+        id
+        attributes {
+          slug
+          title
+          category
+          image {
+            data {
+              attributes {
+                url
+                blurhash
+              }
             }
           }
-        }
-        author_name
-        author_photo {
-          data {
-            attributes {
-              url
-              blurhash
+          author_name
+          author_photo {
+            data {
+              attributes {
+                url
+                blurhash
+              }
             }
           }
         }
       }
     }
-  }
-  successVideos: videos (
-    sort: "createdAt:desc" 
-    pagination: {limit: 2}   
-    filters: {category: {eq: "success"}}
-  ) { 
-    data {
-      attributes {
-        title
-        category 
-        urlEmbed
-        tags
-        urlShare
-        onMainPage 
-      }
-    }
-  }
-  successArticles: articles (
-    sort: "createdAt:desc"
-    pagination: {limit: 3}   
-    filters: {category: {eq: "success"}}
-  ) { 
-    data {
-      id
-      attributes {
-        slug
-        title        
-        category
-        image {
-          data {
-            attributes {
-              url
-              blurhash
-            }
-          }
-        }
-        author_name
-        author_photo {
-          data {
-            attributes {
-              url
-              blurhash
-            }
-          }
+    opportunitiesVideos: videos(
+      sort: "createdAt:desc"
+      pagination: { limit: 2 }
+      filters: { category: { eq: "opportunities" } }
+    ) {
+      data {
+        attributes {
+          title
+          category
+          urlEmbed
+          tags
+          urlShare
+          onMainPage
         }
       }
     }
-  }
-  onMainPageVideos: videos (
-    
-    filters: {onMainPage : {eq: true}}
-  ) { 
-    data {
-      attributes {
-        title
-        category 
-        urlEmbed
-        tags
-        urlShare
-        onMainPage 
+    opportunitiesArticles: articles(
+      sort: "createdAt:desc"
+      pagination: { limit: 3 }
+      filters: { category: { eq: "opportunities" } }
+    ) {
+      data {
+        id
+        attributes {
+          slug
+          title
+          category
+          image {
+            data {
+              attributes {
+                url
+                blurhash
+              }
+            }
+          }
+          author_name
+          author_photo {
+            data {
+              attributes {
+                url
+                blurhash
+              }
+            }
+          }
+        }
       }
     }
-  }   
-}
-
+    successVideos: videos(
+      sort: "createdAt:desc"
+      pagination: { limit: 2 }
+      filters: { category: { eq: "success" } }
+    ) {
+      data {
+        attributes {
+          title
+          category
+          urlEmbed
+          tags
+          urlShare
+          onMainPage
+        }
+      }
+    }
+    successArticles: articles(
+      sort: "createdAt:desc"
+      pagination: { limit: 3 }
+      filters: { category: { eq: "success" } }
+    ) {
+      data {
+        id
+        attributes {
+          slug
+          title
+          category
+          image {
+            data {
+              attributes {
+                url
+                blurhash
+              }
+            }
+          }
+          author_name
+          author_photo {
+            data {
+              attributes {
+                url
+                blurhash
+              }
+            }
+          }
+        }
+      }
+    }
+    onMainPageVideos: videos(filters: { onMainPage: { eq: true } }) {
+      data {
+        attributes {
+          title
+          category
+          urlEmbed
+          tags
+          urlShare
+          onMainPage
+        }
+      }
+    }
+  }
 `;
